569b465705360ef9570e7a8fd2af25ee903ff25e
unknown <775071305@qq.com>
2019-12-01 13:58:00 +0800

    基本echarts使用

--- guli/react-admin-client/src/containers/charts/bar.jsx
+++ guli/react-admin-client/src/containers/charts/bar.jsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import ReactEcharts from "echarts-for-react";
+
 export default function Bar () {
-  return <>Bar  </>
+  function getOptions () {
+    const option = {
+      legend: {
+        data: ['category']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        name: 'category',
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar'
+      }]
+    };
+    return option
+  }
+  return <>
+    <ReactEcharts
+      option={getOptions()}
+    />  </>
 }
--- guli/react-admin-client/src/containers/charts/line.jsx
+++ guli/react-admin-client/src/containers/charts/line.jsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import ReactEcharts from "echarts-for-react";
+
 export default function Line () {
-  return <>  Line</>
+  function getOptions () {
+    const option = {
+      legend: {
+        data: ['category']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        name: 'category',
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'line'
+      }]
+    };
+    return option
+  }
+  return <>
+    <ReactEcharts
+      option={getOptions()}
+    />  </>
 }
